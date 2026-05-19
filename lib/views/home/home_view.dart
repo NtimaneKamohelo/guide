3b8f9a8a3e022427d1db309cea4b5be0e65fddc1
Kamohelo Ntimane <ntimanevincent8@gmail.com>
2026-05-19 21:25:43 +0200

    Created a call to action button

--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:guide/widgets/call_to_action/call_to_action.dart';
 import 'package:guide/widgets/centered_view/centered_view.dart';
 import 'package:guide/widgets/course_details/course_details.dart';
 import 'package:guide/widgets/navigation_bar/navigation_bar.dart';
@@ -17,7 +18,10 @@ class HomeView extends StatelessWidget {
             NavBar(),
             Expanded(
               child: Row(
-                children: <Widget>[CourseDetails()]
+                children: <Widget>[
+                  CourseDetails(),
+                  Expanded(child: Center(child: CallToAction(title: 'Join Course'),),)  
+                ]
               ),
             ),
           ],
